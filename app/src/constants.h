5ce1600e43bf67247ab49dafdab8b3e4b2cfaf54
neithanmo <neithanmo@gmail.com>
2024-11-25 18:47:09 +0100

    Move bec32 constants to own file and add defs for asset encoding
    import bool
    
    Add asset encoding api

--- app/src/constants.h
+++ app/src/constants.h
@@ -29,27 +29,19 @@
 #define SEPARATOR_LENGTH 1
 
 // Some defines for address and asset encoding
-#define ADDR_BECH32_PREFIX "penumbra"
+#define ADDR_BECH32_PREFIX "penumbra" BECH32_SEPARATOR
 // #define FIXED_ADDR_PREFIX ADDR_BECH32_PREFIX BECH32_SEPARATOR
-#define ASSET_BECH32_PREFIX "passet"
+#define ASSET_BECH32_PREFIX "passet" BECH32_SEPARATOR
 #define ASSET_ID_LEN 32
 // HRP length + 1 (separator) + 52 (data) + 6 (checksum) + 1 (null terminator)
-// 6 + 1 + 52 + 6 + 2 = 67
+// 6 + 1 + 52 + 6 + 1 = 66
 #define ENCODED_ASSET_SIZE (strlen(ASSET_BECH32_PREFIX) + ((ASSET_ID_LEN * BITS_PER_BYTE + BECH32_BITS_PER_CHAR - 1) / BECH32_BITS_PER_CHAR) + CHECKSUM_LENGTH + 1)
+
+#define ADDR_HRP_LENGTH (sizeof(ADDR_BECH32_PREFIX) - 1)
 
 #define ENCODED_DATA_LENGTH \
     (((ADDRESS_LEN_BYTES + CHECKSUM_LENGTH) * BITS_PER_BYTE + BECH32_BITS_PER_CHAR - 1) / BECH32_BITS_PER_CHAR)
 
-#define ENCODED_ADDR_LEN (sizeof(ADDR_BECH32_PREFIX) + SEPARATOR_LENGTH + ENCODED_DATA_LENGTH)
+#define ENCODED_ADDR_LEN (ADDR_HRP_LENGTH + SEPARATOR_LENGTH + ENCODED_DATA_LENGTH)
 
 #define ENCODED_ADDR_BUFFER_SIZE (ENCODED_ADDR_LEN + 2)
-
-
-// MEMO transaction constants
-#define MEMO_CIPHERTEXT_LEN_BYTES 528
-
-// This is the `MEMO_CIPHERTEXT_LEN_BYTES` - MAC size (16 bytes).
-#define MEMO_LEN_BYTES 512
-
-// This is the largest text length we can support
-#define MAX_TEXT_LEN MEMO_LEN_BYTES - ADDRESS_LEN_BYTES
