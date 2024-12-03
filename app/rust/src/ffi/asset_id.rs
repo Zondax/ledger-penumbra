36dacc763e31fe8434a893170af367e3d7c3ff94
neithanmo <neithanmo@gmail.com>
2024-12-03 10:31:29 +0100

    Add assetId compuation and enhance api

--- app/rust/src/ffi/asset_id.rs
+++ app/rust/src/ffi/asset_id.rs
@@ -1,8 +1,7 @@
 use crate::constants::ID_LEN_BYTES;
 
-use crate::parser::id::ASSET_ID_PERSONAL;
+use crate::parser::id::AssetId;
 use crate::parser::BytesC;
-use crate::utils::prf::expand_fq;
 use crate::ParserError;
 
 /// Retrieves the asset ID from the provided metadata.
@@ -29,24 +28,18 @@ pub unsafe extern "C" fn rs_get_asset_id_from_metadata(
     if out_len as usize != ID_LEN_BYTES {
         return ParserError::InvalidLength as u32;
     }
-    // let id = Id(Fq::from_le_bytes_mod_order(
-    //     // XXX choice of hash function?
-    //     blake2b_simd::Params::default()
-    //         .personal(b"Penumbra_AssetID")
-    //         .hash(base_denom.as_bytes())
-    //         .as_bytes(),
-    // ));
+
     let out = core::slice::from_raw_parts_mut(out, out_len as _);
     let s = core::slice::from_raw_parts(data.ptr, data.len as _);
 
     let Ok(denom) = core::str::from_utf8(s) else {
-        return ParserError::InvalidTypeId as u32;
+        return ParserError::InvalidAssetId as u32;
     };
-    let Ok(fq) = expand_fq::expand_ff(ASSET_ID_PERSONAL, &[], denom.as_bytes()) else {
-        return ParserError::InvalidTypeId as u32;
+    let Ok(asset) = AssetId::new(denom) else {
+        return ParserError::InvalidAssetId as u32;
     };
 
-    out.copy_from_slice(&fq.to_bytes());
+    out.copy_from_slice(asset.to_bytes().as_ref());
 
     ParserError::Ok as u32
 }
